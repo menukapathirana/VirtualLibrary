cee692d29591ccb8b7e4b14ae5bde21d79220fe3
Menuka Pathirana <61132103+menukapathirana@users.noreply.github.com>
2022-03-18 14:49:40 +1100

    Update ExampleUnitTest.kt

--- app/src/test/java/com/munuka/virtuallibrary/ExampleUnitTest.kt
+++ app/src/test/java/com/munuka/virtuallibrary/ExampleUnitTest.kt
@@ -7,7 +7,6 @@ import org.junit.Assert.*
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
     @Test
